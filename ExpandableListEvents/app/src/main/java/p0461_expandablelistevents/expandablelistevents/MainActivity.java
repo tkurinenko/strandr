11f41a9f8a7393484a9432477eb8f24adb4f13c8
Tanya Kurinenko <tanya.kurinenko@gmail.com>
2016-02-08 17:15:16 +0200

    Adapter SetViewText,  SetViewImage methods

---
+++ ExpandableListEvents/app/src/main/java/p0461_expandablelistevents/expandablelistevents/MainActivity.java
@@ -0,0 +1,82 @@
+package p0461_expandablelistevents.expandablelistevents;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    final String LOG_TAG = "myLogs";
+
+    ExpandableListView elvMain;
+    AdapterHelper ah;
+    SimpleExpandableListAdapter adapter;
+    TextView tvInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        tvInfo = (TextView) findViewById(R.id.tvInfo);
+
+        // создаем адаптер
+        ah = new AdapterHelper(this);
+        adapter = ah.getAdapter();
+
+        elvMain = (ExpandableListView) findViewById(R.id.elvMain);
+        elvMain.setAdapter(adapter);
+
+        // нажатие на элемент
+        elvMain.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition,   int childPosition, long id) {
+                Log.d(LOG_TAG, "onChildClick groupPosition = " + groupPosition +
+                        " childPosition = " + childPosition +
+                        " id = " + id);
+                tvInfo.setText(ah.getGroupChildText(groupPosition, childPosition));
+                return false;
+            }
+        });
+
+
+        elvMain.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                Log.d(LOG_TAG, "onGroupClick groupPosition = " + groupPosition +
+                        " id = " + id);
+                // блокируем дальнейшую обработку события для группы с позицией 1
+                if (groupPosition == 1) return true;
+
+                return false;
+            }
+        });
+
+        // сворачивание группы
+        elvMain.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            public void onGroupCollapse(int groupPosition) {
+                Log.d(LOG_TAG, "onGroupCollapse groupPosition = " + groupPosition);
+                tvInfo.setText("Свернули " + ah.getGroupText(groupPosition));
+            }
+        });
+
+        elvMain.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            public void onGroupExpand(int groupPosition) {
+                Log.d(LOG_TAG, "onGroupExpand groupPosition = " + groupPosition);
+                tvInfo.setText("Равзвернули " + ah.getGroupText(groupPosition));
+            }
+        });
+
+        elvMain.expandGroup(2);
+
+
+    }
+
+
+}
